4e5b05cad3ffef25ddff3bb4c2444198e396b426
Viaan2310 <singhal.viaan13@gmail.com>
2025-11-28 09:34:59 +0530

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Switch, Route } from "wouter";
+import Home from "./pages/home";
+
+function App() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+    </Switch>
+  );
+}
+
+export default App;
